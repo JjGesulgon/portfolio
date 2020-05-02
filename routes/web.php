7e2a61655b2ff0df6376f5804b4e373c7b410357
jjgesulgon <jj@hybrain.co>
2020-05-03 02:53:38 +0800

    auth routes bug fix

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,24 @@ Route::group(['namespace' => 'Admin', 'domain' => 'admin.portfolio.test'], funct
         Route::post('/logout', [ 'as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
         Route::get('/password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm');
         Route::post('/password/confirm', 'Auth\ConfirmPasswordController@confirm');
-        Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-        Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-        Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
-        Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-        Route::get('/register', 'Auth\RegisterController@showRegistationForm');
-        Route::post('/register', 'Auth\RegisterController@register');
+        // Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        // Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+        // Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.reset');
+        // Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('show-password-reset-form');
+        // Route::get('/register', 'Auth\RegisterController@showRegistationForm')->name('show-registration-form');
+        // Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+        // Forget Password
+        Route::get('/password/reset/', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('/password/email/', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+        // Reset Password
+        Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('show-password-reset-form');
+        Route::post('/password/reset/', 'Auth\ResetPasswordController@reset')->name('password.reset');
+
+        // Register
+        Route::get('register/', 'Auth\RegisterController@showRegistrationForm')->name('show-registration-form');
+        Route::post('register/', 'Auth\RegisterController@register')->name('register');
 
         Route::get('/{any}', 'SpaController@index')->where('any', '.*')->name('admins.index');
 
