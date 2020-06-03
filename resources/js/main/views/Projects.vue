d0894795689808e44519427a9516d15612b37cba
jjgesulgon <jj@hybrain.co>
2020-06-03 12:38:32 +0800

    projects module

--- resources/js/main/views/Projects.vue
+++ resources/js/main/views/Projects.vue
@@ -9,13 +9,46 @@
                 <p>What I've been up to.</p>
             </div>
         </div>
-        <div v-for="project in projects" v-bind:key="project.name" class="mt-5">
+        <div v-for="project in projects" v-bind:key="project.name" class="project-container">
+            <div class="text-center project-title mb-3">
+                {{ project.name }}
+            </div>
             <b-container class="bv-example-row mb-5">
                 <b-row>
                     <b-col sm>
                         <b-img :src="`/storage/images/${project.image}`" fluid alt="Responsive image"></b-img>
                     </b-col>
-                    <b-col sm class="text-center">{{ project.name }}</b-col>
+                    <b-col sm>
+                        <div class="mt-2">
+                            <b-row>
+                                <b-col cols="4" class="project-property-label">Description:</b-col>
+                                <b-col cols="8">
+                                    <div v-html="project.description"></div>
+                                </b-col>
+                            </b-row>
+                            <b-row class="mb-2">
+                                <b-col cols="4" class="project-property-label">Role:</b-col>
+                                <b-col cols="8">
+                                    {{ project.role}}
+                                </b-col>
+                            </b-row>
+                            <b-row class="mb-2">
+                                <b-col cols="4" class="project-property-label">Repository Link:</b-col>
+                                <b-col cols="8" v-if="project.github_link != 'n/a'">
+                                    <a :href="project.github_link">{{ project.github_link}}</a>
+                                </b-col>
+                                <b-col cols="8" v-else>
+                                    Unavailable due to company policy.
+                                </b-col>
+                            </b-row>
+                            <b-row class="mb-2">
+                                <b-col cols="4" class="project-property-label">Live Link:</b-col>
+                                <b-col cols="8">
+                                    <a :href="project.live_link">{{ project.live_link}}</a>
+                                </b-col>
+                            </b-row>
+                        </div>
+                    </b-col>
                 </b-row>
             </b-container>
         </div>
@@ -43,6 +76,20 @@
     }
     .sub-intro{
         background-color: #dbdbdb;
+    }
+
+    .project-title{
+        font-weight: bolder;
+        font-size: 20px;
+    }
+
+    .project-property-label{
+        font-weight: bold;
+        text-align: right;
+    }
+
+    .project-container{
+        margin-top: 5em !important;
     }
 </style>
 <script>
