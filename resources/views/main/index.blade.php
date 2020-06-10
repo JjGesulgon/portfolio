477c6c00c13fbfc11e4f308a6c872e173eec1ba2
jjgesulgon <jj@hybrain.co>
2020-06-10 13:01:28 +0800

    favicon

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -31,6 +31,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ URL::asset('/assets/favicon.ico') }}" type="image/x-icon"/>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
