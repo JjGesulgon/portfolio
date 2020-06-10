67dc15439b40a430777d40bdbe293b4b5ea9ea77
jjgesulgon <jj@hybrain.co>
2020-06-10 14:29:00 +0800

    meta

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -25,8 +25,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+    <meta property="og:title" content="Jj Gesulgon Personal Website" />
+    <meta property="og:description" content="Fullstack Web Developer and Software Engineer" />
+    <meta property="og:image" content="{{ URL::asset('/assets/meta_image.jpg') }}" />
+    <meta property="og:image:width" content="600" />
+    <meta property="og:image:height" content="600" />
+    <meta property="og:url" content="https://www.jjgesulgon.dev" />
+    <meta property="og:site_name" content="Jj Gesulgon Website" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:author" content="Jj Gesulgon" />
 
-    <script src="https://kit.fontawesome.com/ce4ba49603.js" crossorigin="anonymous"></script>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -39,23 +49,13 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
-    <meta property="og:title" content="Jj Gesulgon Personal Website" />
-    <meta property="og:description" content="Fullstack Web Developer and Software Engineer" />
-    <meta property="og:image" content="{{ URL::asset('/assets/meta_image.jpg') }}" />
-    <meta property="og:image:width" content="600" />
-    <meta property="og:image:height" content="600" />
-    <meta property="og:url" content="https://www.jjgesulgon.dev" />
-    <meta property="og:site_name" content="Jj Gesulgon Website" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" />
-    <meta property="og:author" content="Jj Gesulgon" />
 </head>
 <body>
     <div id="app">
         <app></app>
     </div>
+
+    <script src="https://kit.fontawesome.com/ce4ba49603.js" crossorigin="anonymous"></script>
 
     <script src="/js/manifest.js"></script>
     <script src="/js/vendor.js"></script>
